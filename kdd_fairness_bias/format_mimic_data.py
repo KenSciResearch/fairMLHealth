05e591cdf41e78fb173f822b78dbd4e326feb1d8
camagallen <ca.magallen@gmail.com>
2020-08-12 09:42:21 -0700

    filled empty docstrings and

--- kdd_fairness_bias/format_mimic_data.py
+++ kdd_fairness_bias/format_mimic_data.py
@@ -1,7 +1,7 @@
-'''
-## NOTE: Pandas can directly read zipped csv files, i.e. df = pd.read_csv('filename.zip')
-
-'''
+"""
+    Formats MIMIC-III data for use with the KDD 2020 Tutorial on Fairness and Bias 
+    in Healthcare
+"""
 
 import io
 import os
@@ -13,9 +13,13 @@ from sklearn import preprocessing
 
 
 def custom_round(col, base=5, sig_dec=0):
-    '''
-        :arg sig_dec: number of significant decimals for the custom-rounded value
-    '''
+    """ Returns the column with values rounded per the custom base value
+
+        Args:
+            col (str): name of column to round
+            base (float): base value to which data should be rounded (may be decimal)
+            sig_dec (int): number of significant decimals for the custom-rounded value
+    """
     assert base >= 0.01, (
         f"cannot round with base {base}. custom_round designed for base>=0.01.")
     result = col.apply(lambda x: round(base * round(float(x)/base), sig_dec))
@@ -23,6 +27,11 @@ def custom_round(col, base=5, sig_dec=0):
 
 
 def load_icd_ccs_xwalk(code_type):
+    """ Returns dataframe containing an ICD-9 to CCS crosswalk
+    
+        Args:
+            code_type (str): key for file location (one of 'dx' or 'px')
+    """
     urls = {'dx':"https://data.nber.org/ahrq/ccs/dxicd2ccsxw.csv",
             'px':"https://data.nber.org/ahrq/ccs/pricd2ccsxw.csv"
             }
@@ -36,12 +45,19 @@ def load_icd_ccs_xwalk(code_type):
 
 
 class mimic_loader():
+    """ Generates a MIMIC-III data subset formatted for use with the KDD Tutorial
+    """
     def __init__(self, data_file=""):
         self.output_file = os.path.expanduser(data_file)
-        self.data_dir = os.path.dirname(self.output_file)
-        if self.data_dir != "":
+        mimic_dir = os.path.dirname(self.output_file)
+        if mimic_dir != "":
             assert os.path.exists(self.data_dir), (
                 f"Invalid mimic data directory passed: {self.data_dir}")
+        self.data_dir = os.path.join(mimic_dir, 'zipped_files')
+        assert os.path.exists(self.data_dir), (
+            "MIMIC directory must contain the folder \'zipped_files\',",
+            "which should be present in the raw download")
+
 
     def generate_tutorial_data(self):
         print("Generating Tutorial Data...")
@@ -56,29 +72,34 @@ class mimic_loader():
         df.to_csv(self.output_file, index=False)
         return True
 
+
     def __load_mimic_data(self, data_key):
-        ''' Returns transfer data as pd dataframe after removing the ROW_ID column
+        """ Returns transfer data as pd dataframe after removing the ROW_ID column
                 (unneccessary column that causes problems)
             :arg data_key : key for the filename of interest (see get_file_dict() )
             :arg data_dir : the directory in which the file is stored
                             (default is get_data_dir() )
-        '''
+        """
         file_dict = {'dx':"DIAGNOSES_ICD",
                     'ax':"ADMISSIONS",
                     'px':"PROCEDURES_ICD",
                     'rx':"PRESCRIPTIONS",
                     'pt':"PATIENTS"
                     }
-        data_file = f"{self.data_dir}/zipped_files/{file_dict[data_key]}.csv.gz"
+        data_file = f"{self.data_dir}/{file_dict[data_key]}.csv.gz"
         df = pd.read_csv(data_file, low_memory=False)
         if 'ROW_ID' in df.columns:
             del df['ROW_ID']
         return df
-    
+
+
     def load_admit_dscg_data(self):
-        '''
-            :Note: drops data for patients with  age >120 y.o. or age < 0
-        '''
+        """ Loads and returns a dataframe of formatted demographic
+            and length-of-stay data available through admission and
+            discharge tables
+
+            Note: drops data for patients with  age>120 y.o. or age<0
+        """
         admissions = self.__load_mimic_data("ax")
         # Load patient information and use it to calculate age
         dob = self.__load_mimic_data('pt')[['SUBJECT_ID', 'DOB', 'GENDER']]
@@ -125,8 +146,12 @@ class mimic_loader():
         return output
 
     def load_dxpx_data(self, feature_type):
-        ''' Returns a pandas dataframe with formatted diagnosis information
-        '''
+        """ Returns a pandas dataframe with formatted diagnosis or procedure data,
+                categorized in single-level CCS codes and one-hot encoded
+
+            Args:
+                feature_type (str): one of ['dx', 'px'] (i.e. ["diagnosis", "procedure"])
+        """
         ftypes = ['dx','px']
         assert feature_type in ftypes, f"Invalid code_type. Must be one of {ftypes}"
         #
@@ -142,7 +167,8 @@ class mimic_loader():
         assert not df.duplicated().any().any()
         # Reformat data to one-hot encode
         prefix_dict = {'dx':'DIAGNOSIS', 'px':'PROCEDURE'}
-        ohe_df = pd.get_dummies(df[f'{feature_type}_CCS'], prefix=f'{prefix_dict[feature_type]}_CCS')
+        ohe_df = pd.get_dummies(df[f'{feature_type}_CCS'], 
+                                prefix=f'{prefix_dict[feature_type]}_CCS')
         ohe_df['HADM_ID'] = df['HADM_ID']
         agg_dict = {c:'max' for c in ohe_df.columns if c != 'HADM_ID'}
         output = ohe_df.groupby('HADM_ID', as_index=False).agg(agg_dict)
@@ -151,7 +177,6 @@ class mimic_loader():
         assert not output[f'HADM_ID'].duplicated().any(), (
             f"Error loading {feature_type}. Duplicate admissions found in formatting")
         return output
-
 
 
 
--- kdd_fairness_bias/tutorial_helpers.py
+++ kdd_fairness_bias/tutorial_helpers.py
@@ -1,26 +1,38 @@
-'''
-Add-ons for KDD Tutorial on Fairness and Bias in Healthcare
-'''
+"""
+    Add-ons for KDD 2020 Tutorial on Fairness and Bias in Healthcare
+"""
 
 import numpy as np
 import os
 import pandas as pd
 
-# AIF360 Libs
+# AIF360 Libraries
 from aif360.sklearn.metrics import *
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import balanced_accuracy_score, roc_auc_score
 
-# Tutorial Libs
+# Tutorial Libraries
 import format_mimic_data
 
 
 
 
 def get_aif360_measures_df(X_test, y_test, y_pred, y_prob=None, sensitive_attributes=None):
-    ''' Returns a dataframe containing results for each AIF360 measure
-    '''
-    assert isinstance(sensitive_attributes, list) and all([c in X_test.columns for c in sensitive_attributes]), (
-        "sensitive_attributes must be list of columns in X_test")
+    """ Returns a dataframe containing results for the set of AIF360 measures
+        used in the KDD tutorial
+
+        Args:
+            X_test (array-like): Sample features; must include sensitive attribute
+            y_test (array-like, 1-D): Sample targets
+            y_pred (array-like, 1-D): Sample target probabilities
+            sensitive_attributes (list): list of column names or locations in
+                X_test containing the sensitive attribute(s) against which
+                fairness is measured
+    """
+    assert isinstance(sensitive_attributes, list) and all(
+        [c in X_test.columns for c in sensitive_attributes]), (
+            "sensitive_attributes must be list of columns in X_test")
+    if isinstance(X_test, np.ndarray):
+        X_test = pd.DataFrame(X_test)
     if isinstance(y_pred, np.ndarray):
         y_pred = pd.Series(y_pred)
     if isinstance(y_prob, np.ndarray):
@@ -28,43 +40,64 @@ def get_aif360_measures_df(X_test, y_test, y_pred, y_prob=None, sensitive_attrib
             y_prob = y_prob[:, 1]
         y_prob = pd.Series(y_prob)
     #
-    y_test = pd.concat([X_test.loc[:,sensitive_attributes], y_test], axis=1).set_index(sensitive_attributes)
-    y_pred = pd.concat([X_test.loc[:,sensitive_attributes].reset_index(drop=True), y_pred], axis=1).set_index(sensitive_attributes)
-    y_prob = pd.concat([X_test.loc[:,sensitive_attributes].reset_index(drop=True), y_prob], axis=1).set_index(sensitive_attributes)
+    y_test = pd.concat([X_test.loc[:,sensitive_attributes], y_test],
+                           axis=1).set_index(sensitive_attributes)
+    y_pred = pd.concat([X_test.loc[:,sensitive_attributes].reset_index(drop=True),
+                            y_pred], axis=1).set_index(sensitive_attributes)
+    y_prob = pd.concat([X_test.loc[:,sensitive_attributes].reset_index(drop=True),
+                            y_prob], axis=1).set_index(sensitive_attributes)
     y_pred.columns = y_test.columns
     y_prob.columns = y_test.columns
     #
-    print("Generating Scores for Fairness Measures. This may take a few moments...")
     scores = [ ['selection_rate', selection_rate(y_test, y_pred)] ]
     scores.append( ['disparate_impact_ratio',
-                        disparate_impact_ratio(y_test, y_pred, prot_attr=sensitive_attributes)] )
+                        disparate_impact_ratio(y_test, y_pred,
+                                    prot_attr=sensitive_attributes)] )
     scores.append( ['statistical_parity_difference',
-                        statistical_parity_difference(y_test, y_pred, prot_attr=sensitive_attributes)] )
+                        statistical_parity_difference(y_test, y_pred,
+                                    prot_attr=sensitive_attributes)] )
     scores.append( ['average_odds_difference',
-                        average_odds_difference(y_test, y_pred, prot_attr=sensitive_attributes)] )
+                        average_odds_difference(y_test, y_pred,
+                                    prot_attr=sensitive_attributes)] )
     scores.append( ['average_odds_error',
-                        average_odds_error(y_test, y_pred, prot_attr=sensitive_attributes)] )
+                        average_odds_error(y_test, y_pred,
+                                     prot_attr=sensitive_attributes)] )
     scores.append( ['equal_opportunity_difference',
-                        equal_opportunity_difference(y_test, y_pred, prot_attr=sensitive_attributes)] )
-    scores.append( ['generalized_entropy_error', generalized_entropy_error(y_test.iloc[:,0], y_pred.iloc[:,0])] )
+                        equal_opportunity_difference(y_test, y_pred,
+                                    prot_attr=sensitive_attributes)] )
+    scores.append( ['generalized_entropy_error',
+                        generalized_entropy_error(y_test.iloc[:,0], y_pred.iloc[:,0])] )
     scores.append( ['between_group_generalized_entropy_error',
-                        between_group_generalized_entropy_error(y_test, y_pred, prot_attr=sensitive_attributes)] )
+                        between_group_generalized_entropy_error(y_test, y_pred,
+                                    prot_attr=sensitive_attributes)] )
     scores.append( ['consistency_score', consistency_score(X_test, y_pred)] )
     if y_prob is not None:
         scores.append( ['Between-Group AUC Difference',
-                        difference(roc_auc_score, y_test, y_prob, prot_attr=sensitive_attributes, priv_group=1)] )
+                        difference(roc_auc_score, y_test, y_prob,
+                                   prot_attr=sensitive_attributes, priv_group=1)] )
+        scores.append( ['Between-Group Balanced Accuracy Difference',
+                        difference(balanced_accuracy_score, y_test, y_pred,
+                                   prot_attr=sensitive_attributes, priv_group=1)] )
     #
     model_scores =  pd.DataFrame(scores, columns=['measure','value'])
     return(model_scores)
 
 
+
+
 def load_example_data(mimic_dirpath):
-    """ 
+    """ Returns a formatted MIMIC-III data subset for use in KDD Tutorial
+
+        If formatted data file exists, loads that file. Else, generates
+        formatted data and saves in mimic_dirpath.
+
+        Args:
+            mimic_dirpath (str): valid path to downloaded MIMIC data
     """
     data_file = os.path.join(os.path.expanduser(mimic_dirpath), "kdd_tutorial_data.csv")
     if not os.path.exists(data_file):
-        fmd = format_mimic_data.mimic_loader(data_file)
-        success = fmd.generate_tutorial_data()
+        formatter = format_mimic_data.mimic_loader(data_file)
+        success = formatter.generate_tutorial_data()
         assert success, "Error generating tutorial data."
     else:
         pass
@@ -74,3 +107,4 @@ def load_example_data(mimic_dirpath):
     df.rename(columns={'HADM_ID':'ADMIT_ID'}, inplace=True)
     df = df.loc[df['AGE'].ge(65),:]
     return(df)
+
