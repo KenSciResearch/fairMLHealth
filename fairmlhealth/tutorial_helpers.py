30b5d9384160e930b3d48de5c4267ae9b283f5e9
camagallen <ca.magallen@gmail.com>
2021-03-15 12:12:03 -0700

    perf: revert to incl. feat. printout; use native LANGUAGE_ENGL feature

--- fairmlhealth/tutorial_helpers.py
+++ fairmlhealth/tutorial_helpers.py
@@ -10,18 +10,8 @@ Contributors:
 # Copyright (c) KenSci and contributors.
 # Licensed under the MIT License.
 
-from IPython.display import display
-import numpy as np
-import os
-import pandas as pd
-
-# Metric libraries
-from aif360.sklearn.metrics import *
-from sklearn.metrics import (
-    balanced_accuracy_score, roc_auc_score, accuracy_score, precision_score)
-
-# Tutorial Libraries
 from . import mimic_data
+import pandas as pd
 
 
 '''
@@ -102,8 +92,6 @@ def highlight_vals(df, values, colname=None, criteria=None, color='magenta',
                 if highlight.any() else '' for v in highlight]
 
 
-
-
 '''
 Loaders and Printers
 '''
@@ -113,7 +101,7 @@ def load_mimic3_example(mimic_dirpath):
     return mimic_data.load_mimic3_example(mimic_dirpath)
 
 
-def print_feature_table(df):
+def feature_table(df):
     ''' Displays a table containing statistics on the features available in the
             passed df
 
@@ -122,14 +110,15 @@ def print_feature_table(df):
     '''
     print(f"\n This data subset has {df.shape[0]} total observations" +
           f" and {df.shape[1]-2} input features \n")
-    feat_df = pd.DataFrame({'feature': df.columns.tolist()}
-               ).query('feature not in ["ADMIT_ID", "length_of_stay"]')
+    feat_df = pd.DataFrame({'feature': df.columns.tolist()
+                            }).query(
+                                'feature not in ["ADMIT_ID", "length_of_stay"]')
     feat_df['Raw Feature'] = feat_df['feature'].str.split("_").str[0]
     count_df = feat_df.groupby('Raw Feature', as_index=False
                                )['feature'].count(
                      ).rename(columns={
-                              'feature': 'Category Count (Encoded Features)'})
-    display(count_df)
+                              'feature': 'Category Count (Encoded Features).'})
+    return count_df
 
 
 '''
