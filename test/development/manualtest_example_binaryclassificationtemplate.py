a1d6665172fff269327fcb0841c34308d62416c6
camagallen <ca.magallen@gmail.com>
2021-04-14 17:21:16 -0700

    perf: automated test for example template now run manually d.t. extended processing time

---
+++ test/development/manualtest_example_binaryclassificationtemplate.py
@@ -0,0 +1,36 @@
+'''
+Method adapted from:
+http://www.christianmoscardi.com/blog/2016/01/20/jupyter-testing.html
+
+Note: because the models for the notebook being tested may take hours to run,
+this test must be run manually. However, since other notebooks rely on the same
+functions it's very unlikely
+'''
+
+from .. import notebook_tester as nbtest
+import os
+import warnings
+
+
+def test_example_binaryclassificationtemplate():
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    nb = "Example-BinaryClassificationTemplate.ipynb"
+    nb_path = os.path.join(this_dir, "..", "..", "tutorials_and_examples", nb)
+    _, err = nbtest.validate_notebook(nb_path, timeout=1800)
+
+    if any(err):
+        for e in err:
+            for t in e['traceback']:
+                print(t)
+        raise AssertionError("Notebook Broken")
+    else:
+        pass
+
+    warns = nbtest.list_warnings(nb)
+    if any(warns):
+        for t in warns:
+            if isinstance(t['text'], list):
+                wrn = t['text'][0]
+            else:
+                wrn = t['text']
+            warnings.warn(wrn)
