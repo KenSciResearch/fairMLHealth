eee4fd0b2fff310583f9149891b3c25c0285e13c
camagallen <ca.magallen@gmail.com>
2021-04-16 10:04:19 -0700

    feat: find available kernels

--- test/notebook_tester.py
+++ test/notebook_tester.py
@@ -1,45 +1,52 @@
 '''
-Adapted from:
+Method Adapted from:
 http://www.christianmoscardi.com/blog/2016/01/20/jupyter-testing.html
 
 Upon recommendation from the AIF360 development team
 (http://aif360.mybluemix.net/)
 '''
 
-
+import json
 import nbformat
 import os
 import subprocess
 import tempfile
 
 
-def validate_notebook(path, timeout=60):
-    """ Executes the notebook via nbconvert and collects the output
+
+def find_kernel(nb_file=None):
+    """ Determines which, if any, jupyter kernels are available.
 
     Args:
-        path (string): path to the notebook of interest
-        timeout (int): max allowed time (in seconds)
+        nb_file (str filepath, optional): If present, find_kernel will parse for
+        the notebook's default kernel name and check to see if it's among the
+        available kernels. Defaults to None.
 
     Returns:
-        (parsed nbformat.NotebookNode object, list of execution errors)
+        str: name of a viable kernel
     """
-    dirname, __ = os.path.split(path)
-    os.chdir(dirname)
+    if nb_file is not None:
+        with open(nb_file) as json_file:
+            contents = json.load(json_file)
+            if 'kernelspec' in contents['metadata'].keys():
+                kname = contents['metadata']['kernelspec']['name']
+            else:
+                kname = None
 
-    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
-        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
-        f"--ExecutePreprocessor.timeout={timeout}",
-        "--ExecutePreprocessor.allow_errors=True",
-        "--output", fout.name, path]
-
-        subprocess.check_call(args)
-
-        fout.seek(0)
-        nb = nbformat.read(fout, nbformat.current_nbformat)
-
-    errors = list_errors(nb)
-
-    return nb, errors
+    args = ["jupyter", "kernelspec", "list"]
+    try:
+        kernels = subprocess.check_output(args).decode("utf-8")
+        # If any kernels are available, the first item in post-split list will
+        # be "Available kernels" (hence truncate)
+        kernels = kernels.split("\n")[1:]
+        available_kernels = [k.split()[0] for k in kernels if k.split()]
+        if kname is None or kname not in available_kernels:
+            kname = available_kernels[0]
+        return kname
+    except OSError:
+        return None
+    except BaseException as e:
+        raise Exception(e)
 
 
 def list_errors(nb):
@@ -71,3 +78,36 @@ def list_warnings(nb):
 
     return warns
 
+
+def validate_notebook(nb_path, timeout=60):
+    """ Executes the notebook via nbconvert and collects the output
+
+    Args:
+        nb_path (string): path to the notebook of interest
+        timeout (int): max allowed time (in seconds)
+
+    Returns:
+        (parsed nbformat.NotebookNode object, list of execution errors)
+    """
+    dirname, __ = os.path.split(nb_path)
+    os.chdir(dirname)
+
+    kname = find_kernel(nb_path)
+    if kname is None:
+        raise OSError("No kernel found")
+
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+        f"--ExecutePreprocessor.timeout={timeout}",
+        f"--ExecutePreprocessor.kernel_name={kname}",
+        "--ExecutePreprocessor.allow_errors=True",
+        "--output", fout.name, nb_path]
+
+        subprocess.check_call(args)
+
+        fout.seek(0)
+        nb = nbformat.read(fout, nbformat.current_nbformat)
+
+    errors = list_errors(nb)
+
+    return nb, errors
