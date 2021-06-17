6b51522b3fe36ea9eab2fbb1be7e345aee8dc241
camagallen <ca.magallen@gmail.com>
2021-06-17 11:55:27 -0700

    test: add escapes

--- win32workaround.sh
+++ win32workaround.sh
@@ -4,4 +4,4 @@ python3 -m pip install pywin32 --upgrade
 PYLOC=$(where python | head -n 1)
 PYLOC=$(dirname $PYLOC)
 ls $PYLOC
-python3 ${PYLOC}\Lib\site-packages\win32\pywin32_postinstall.py -install
+python3 ${PYLOC}\\Lib\\site-packages\\win32\\pywin32_postinstall.py -install
