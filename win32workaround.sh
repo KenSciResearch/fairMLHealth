efd2820526686ac74d38dfd0d7d75982fdbbb4ae
camagallen <ca.magallen@gmail.com>
2021-06-17 11:44:56 -0700

    test: attempt as bash script

---
+++ win32workaround.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Workaround to enable use of win32 on azure CI pipeline vm
+python3 -m pip install pywin32 --upgrade
+PYLOC=$(where python | dirname head -n 1)
+PYLOC=$(dirname $PYLOC)
+ls $PYLOC
+python3 ${PYLOC}\Lib\site-packages\win32\pywin32_postinstall.py -install
