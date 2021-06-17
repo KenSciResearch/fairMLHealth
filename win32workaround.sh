7b78f693b7daa4df3347d983709c0583c93d2c46
camagallen <ca.magallen@gmail.com>
2021-06-17 12:01:22 -0700

    test: alternate postinstall location

--- win32workaround.sh
+++ win32workaround.sh
@@ -3,5 +3,4 @@
 python3 -m pip install pywin32 --upgrade
 PYLOC=$(where python | head -n 1)
 PYLOC=$(dirname $PYLOC)
-ls $PYLOC
-python3 ${PYLOC}\\Lib\\site-packages\\win32\\pywin32_postinstall.py -install
+python3 ${PYLOC}\\Scripts\\pywin32_postinstall.py -install
