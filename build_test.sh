568f453de1ef74a2e9ba9fb81bd8b8373b8723a4
camagallen <ca.magallen@gmail.com>
2021-06-17 10:07:21 -0700

    test: test workaround via pipeline setup

--- build_test.sh
+++ build_test.sh
@@ -10,6 +10,5 @@ export IS_CICD=true
 
 # Force install of test dependencies before running pytest (should happen
 #   automatically but sometimes fails)
-if [["$OSTYPE" == "win32"]]; then python3 -m pip install pywin32 --upgrade; fi
 python3 -m pip install .[test]
 python3 -m pytest
