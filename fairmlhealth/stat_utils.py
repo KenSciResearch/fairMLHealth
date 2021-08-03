ddc14350a85708619d2cadee1d83864429d40929
camagallen <ca.magallen@gmail.com>
2021-08-02 17:22:22 -0700

    feat: bootstrapping abstracted to accept greater variety of functions

--- fairmlhealth/stat_utils.py
+++ fairmlhealth/stat_utils.py
@@ -1,29 +1,76 @@
 '''
 Supplemental functions that may be useful in analysis or data munging.
 '''
+from numbers import Number
 import numpy as np
 import pandas as pd
-from scipy.stats import kruskal
+from scipy import stats
 from typing import Callable
-from . import __validation as valid
+from . import __preprocessing as prep, __validation as valid
 
 
-def bootstrap_significance(a, b, func:Callable=kruskal, alpha:float=0.05,
-                           n_samples:int=50, n_trials:int=100):
-    """ Applies bootstrapping to evaluate the statistical difference between
-    two samples. Returns True (significant) if the p-value is less than alpha
-    for at least P=1-alpha percent of trials.
+
+
+''' Default P-Value Functions '''
+
+
+def chisquare_pval(group:valid.ArrayLike, values:valid.ArrayLike,
+                   n_samples:Number=50):
+    def smpl(ser:pd.Series, n_samp:int=n_samples):
+        return ser.sample(n=n_samp, replace=True).reset_index(drop=True)
+    #
+    g = prep.prep_arraylike(group, "group", expected_len=None)
+    v = prep.prep_arraylike(values, "values", expected_len=len(g))
+    n = n_samples if n_samples <= g.shape[0] else g.shape[0]
+    data = pd.concat([smpl(g, n), smpl(v, n)], axis=1, ignore_index=True)
+    data.columns=['group', 'values']
+    # Using groupby and unstack to avoid bugs in some versions of crosstab
+    ctab = data.groupby(['group', 'values'])['values'].count().unstack().fillna(0)
+    pval = stats.chi2_contingency(ctab)[1]
+    return pval
+
+
+def kruskal_pval(dist_a:valid.ArrayLike, dist_b:valid.ArrayLike,
+                 n_samples:Number=50):
+    def smpl(arr, n_samp):
+        return np.random.choice(arr, size=n_samp, replace=True)
+    #
+    valid.validate_array(dist_a, expected_len=None)
+    valid.validate_array(dist_b, expected_len=None)
+    min_sample = min(dist_a.shape[0], dist_b.shape[0])
+    n = n_samples if n_samples <= min_sample else min_sample
+    pval = stats.kruskal(smpl(dist_a, n), smpl(dist_b, n))[1]
+    return pval
+
+
+''' Supplemental Functions '''
+
+
+def binary_result_labels(y_true:valid.ArrayLike, y_pred:valid.ArrayLike):
+    y = prep.prep_arraylike(y_true, "y_true", expected_len=None)
+    yh = prep.prep_arraylike(y_pred, "y_prob", expected_len=len(y))
+    valid.__validate_binVal(y, "y_true", fuzzy=False)
+    valid.__validate_binVal(y, "y_pred", fuzzy=False)
+    res = pd.Series([""]*len(y_pred), name="prediction result")
+    res.loc[y.eq(1) & yh.eq(1)] = "TP"
+    res.loc[y.eq(0) & yh.eq(1)] = "FP"
+    res.loc[y.eq(0) & yh.eq(0)] = "TN"
+    res.loc[y.eq(1) & yh.eq(0)] = "FN"
+    return res
+
+
+def bootstrap_significance(alpha:float=0.05, n_trials:int=100,
+                           func:Callable[[], Number]=kruskal_pval, **kwargs):
+    """ Applies bootstrapping to evaluate the frequency of p > alpha. Returns
+        True (significant) if the p-value is less than alpha for at least
+        P=1-alpha percent of trials.
 
     Args:
-        a (array-like): statistical sample.
-        b (array-like): statistical sample for comparison.
-        func (function): any statistical test returning it's p-value as the
-            second member of a tuple.
         alpha (float, optional): Maximum p-value indicating significance.
             Defaults to 0.05.
-        n_samples (int, optional): Number of samples to use for each trial.
-            Defaults to 50.
         n_trials (int, optional): Number of trials to run. Defaults to 100.
+        func (Callable[[kwargs], Number], optional): Any function returning a
+            p-value. Defaults to kruskal_pval.
 
     Returns:
         bool: whether difference is statistically significant
@@ -31,9 +78,7 @@ def bootstrap_significance(a, b, func:Callable=kruskal, alpha:float=0.05,
     # Create a list of p-values for each of n_trials
     pvals = []
     for i in range( 0, n_trials):
-        pvals += [func(np.random.choice(a, size=n_samples, replace=True),
-                       np.random.choice(b, size=n_samples, replace=True))[1]
-                  ]
+        pvals += [func(**kwargs)]
     # Calculate the proportion of trials for which p < alpha
     pvals = [int(v <= alpha) for v in pvals]
     result = bool(np.mean(pvals) >= (1-alpha))
@@ -81,4 +126,3 @@ def feature_table(df:pd.DataFrame):
                      ).rename(columns={
                               'feature': 'Category Count (Encoded Features).'})
     return count_df
-
