da8b5561fd12c326bb9c97ffe1c47248d56605a4
camagallen <ca.magallen@gmail.com>
2021-08-05 02:22:18 -0700

    test: update binary classification example

--- test/test_example_toolusage_binary.py
+++ test/test_example_toolusage_binary.py
@@ -10,7 +10,7 @@ import warnings
 
 def test_example_toolusage():
     this_dir = os.path.dirname(os.path.abspath(__file__))
-    nb_name = "Example-ToolUsage.ipynb"
+    nb_name = "Example-ToolUsage_BinaryClassification.ipynb"
     nb_path = os.path.join(this_dir, "..", "examples_and_tutorials", nb_name)
     nb, err = validate_notebook(nb_path)
 
